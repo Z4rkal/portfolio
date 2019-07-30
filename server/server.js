86a006a62636a8889bb8ee6f45e3b8c764b2ba0e
Michael Fischer <zarkal@live.com>
2019-07-30 12:34:16 -0700

    Adds error catching for sendgrid stuff

--- server/server.js
+++ server/server.js
@@ -3,15 +3,35 @@ const morgan = require('morgan');
 const { json, urlencoded } = require('body-parser');
 const mail = require('@sendgrid/mail');
 
-if (process.env.SENDGRID_API_KEY != undefined)
-    mail.setApiKey(process.env.SENDGRID_API_KEY);
-else
-    mail.setApiKey(require('./sendKey'));
-
-
-const myEmail = process.env.MY_EMAIL != undefined ? process.env.MY_EMAIL : require('./email');
-
 const app = express();
+
+//Try to set up our mail function
+try {
+    mail.setApiKey(process.env.SENDGRID_API_KEY || JSON.stringify(process.env.SENDGRID_API_KEY) || require('./sendKey'));
+    const myEmail = process.env.MY_EMAIL || JSON.stringify(process.env.SENDGRID_API_KEY) || require('./email');
+
+    function handleEmail(body) {
+        const msg = {
+            to: myEmail,
+            from: body.email,
+            subject: 'Contact Request',
+            text: `${body.firstName} ${body.lastName} submitted a contact form.\n\nTheir email is ${body.email}`,
+            html: `<strong>${body.firstName} ${body.lastName} submitted a contact form.\n\nTheir email is ${body.email}</strong>`,
+        };
+
+        mail.send(msg, false, (err, res) => {
+            if (err) {
+                console.log(err);
+                console.log(`\n\n${JSON.stringify(err.response.body.errors)}`)
+            }
+        });
+    }
+    app.locals.noMail = false;
+}
+catch (error) {
+    console.log(error);
+    app.locals.noMail = true;
+}
 
 app.use(morgan('dev'));
 app.use(json());
@@ -27,34 +47,17 @@ app.locals.author =
     {
         firstName: 'Olivia',
         lastName: 'Fischer',
-        pronoun: ['she','her','hers'],
+        pronoun: ['she', 'her', 'hers'],
         gitLink: 'https://github.com/Z4rkal'
     };
-
-
-function handleEmail(body) {
-    const msg = {
-        to: myEmail,
-        from: body.email,
-        subject: 'Contact Request',
-        text: `${body.firstName} ${body.lastName} submitted a contact form.\n\nTheir email is ${body.email}`,
-        html: `<strong>${body.firstName} ${body.lastName} submitted a contact form.\n\nTheir email is ${body.email}</strong>`,
-    };
-
-    mail.send(msg, false, (err, res) => {
-        if (err) {
-            console.log(err);
-            console.log(`\n\n${JSON.stringify(err.response.body.errors)}`)
-        }
-    });
-}
 
 app.get('/contact', (req, res) => {
     res.render('contact');
 });
 
 app.post('/thanks', (req, res) => {
-    handleEmail(req.body);
+    if (!app.locals.noMail)
+        handleEmail(req.body);
 
     res.render('thanks', { contact: req.body })
 });
